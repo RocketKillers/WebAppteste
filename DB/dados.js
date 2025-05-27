25368cba790b7e075a5722c320329586aa3e5660
RocketKillers <jlevoratofilho@gmail.com>
2025-05-27 08:43:40 -0300

    teste

---
+++ DB/dados.js
@@ -0,0 +1,42 @@
+{
+  "clientes": [
+    {
+      "id": "1",
+      "cpf": "111.111.111-11",
+      "nome": "jorge",
+      "telefone": "(18)91111-1111",
+      "cidade": "presidente prudente",
+      "uf": "SP",
+      "cep": "19015-010"
+    },
+    {
+      "id": "2",
+      "cpf": "222.222.222-22",
+      "nome": "aldemir",
+      "telefone": "(12)92222-2222",
+      "cidade": "ribeirão preto",
+      "uf": "SP",
+      "cep": "15120-153"
+    },
+    {
+      "id": "3",
+      "cpf": "333.333.333-33",
+      "nome": "wesley",
+      "telefone": "(51)93333-3333",
+      "cidade": "salvador",
+      "uf": "BA",
+      "cep": "18156-521"
+    },
+    {
+      "id": "4",
+      "cpf": "444.444.444-44",
+      "nome": "jason vorghess",
+      "telefone": "(55)94444-4444",
+      "cidade": "chique-chique",
+      "uf": "BA",
+      "cep": "10215-200"
+    }
+  ],
+  "fornecedores": [],
+  "produtos": []
+}
